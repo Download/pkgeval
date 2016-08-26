2a6d2a22cfe26fd57f94d88da7aec968b86501d0
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-08-26 20:19:38 +0200

    0.1.0: Initial version

---
+++ pkgeval.js
@@ -0,0 +1,14 @@
+﻿var pkgcfg = require('pkgcfg');
+
+function pkgeval(pkg, node, expr) {
+	var result = '';
+	if (expr) {
+		try {result = eval(expr)}
+		catch(e) {throw new pkgcfg.QuietError('Error evaluating expression (' + expr + '): ' + e.message);}
+	}
+	return result;
+}
+
+pkgcfg.registry.register('eval', pkgeval);
+
+module.exports = pkgeval;
---
+++ pkgeval.spec.js
@@ -0,0 +1,28 @@
+﻿var log = require('picolog');
+var pkgcfg = require('pkgcfg');
+var pkgeval = require('./pkgeval');
+var expect = require('chai').expect;
+
+describe('pkgeval(pkg, node, expr)', function(){
+	it('is a function', function(){
+		expect(pkgeval).to.be.a('function');
+	});
+
+	it('registers itself with `pkgcfg` under tag `{eval}`.', function(){
+		expect(pkgcfg.registry.getTransform('eval')).to.equal(pkgeval);
+	});
+
+	describe('{eval <expr>}', function(){
+		var pkg = pkgcfg();
+
+		// "eval": "{eval 3 > 5}",
+		it('Evaluates the expression', function(){
+			expect(pkg.test['eval']).to.equal(false);
+		});
+
+		// "eval-indirect": "{eval {pkg test.data} > 5}",
+		it('Resolves other tags, then evaluates the resulting expression', function(){
+			expect(pkg.test['eval-indirect']).to.equal(true);
+		});
+	});
+});
