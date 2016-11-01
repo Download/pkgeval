2f9ded12a523905c0ac354a4f4f5ac0282495157
Stijn de Witt <StijnDeWitt@hotmail.com>
2016-11-01 02:44:51 +0100

    0.8.0: Updated dependencies, changed logger ``` mocha    3.0.2  ==>  mocha    3.1.2 pkgcfg   0.7.0  ==>  pkgcfg   0.8.1 picolog  1.0.4  ==>  ulog     1.0.0 ```

--- pkgeval.js
+++ pkgeval.js
@@ -1,7 +1,10 @@
-﻿var pkgcfg = require('pkgcfg');
+﻿var log; try {log = require('ulog')('pkgeval')} catch(e) {}
+
+var pkgcfg = require('pkgcfg');
 
 function pkgeval(pkg, node, expr) {
 	if (expr) {
+		log && log.log(log.name + ': evaluating code: ' + expr)
 		try {return eval(expr)}
 		catch(e) {throw new Error('Error evaluating expression (' + expr + '): ' + e.message);}
 	}
@@ -9,3 +12,5 @@ function pkgeval(pkg, node, expr) {
 
 pkgcfg.registry.register('eval', pkgeval);
 module.exports = pkgeval;
+
+log && log.log('Initialized ' + log.name)
--- pkgeval.spec.js
+++ pkgeval.spec.js
@@ -1,28 +1,33 @@
-﻿var log = require('picolog');
-var pkgcfg = require('pkgcfg');
-var pkgeval = require('./pkgeval');
-var expect = require('chai').expect;
+﻿var log = require('ulog')('pkgeval:spec')
+
+var pkgcfg = require('pkgcfg')
+var pkgeval = require('./pkgeval')
+var expect = require('chai').expect
 
 describe('pkgeval(pkg, node, expr)', function(){
 	it('is a function', function(){
-		expect(pkgeval).to.be.a('function');
-	});
+		log.log('is a function: ', pkgeval)
+		expect(pkgeval).to.be.a('function')
+	})
 
 	it('registers itself with `pkgcfg` under tag `{eval}`.', function(){
-		expect(pkgcfg.registry.getTransform('eval')).to.equal(pkgeval);
-	});
+		log.log('is registered: ', pkgcfg.registry.getTransform('eval'))
+		expect(pkgcfg.registry.getTransform('eval')).to.equal(pkgeval)
+	})
 
 	describe('{eval <expr>}', function(){
-		var pkg = pkgcfg();
+		var pkg = pkgcfg()
 
 		// "eval": "{eval 3 > 5}",
 		it('Evaluates the expression', function(){
-			expect(pkg.test['eval']).to.equal(false);
-		});
+			expect(pkg.test['eval']).to.equal(false)
+		})
 
 		// "eval-indirect": "{eval {pkg test.data} > 5}",
 		it('Resolves other tags, then evaluates the resulting expression', function(){
-			expect(pkg.test['eval-indirect']).to.equal(true);
-		});
-	});
-});
+			expect(pkg.test['eval-indirect']).to.equal(true)
+		})
+	})
+})
+
+log.log('Initialized ' + log.name)
